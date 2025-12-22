40692654f50e1d4b13e916205960bee9f2a9bfbb
QuentinSco <q.scozzafave@gmail.com>
2025-12-22 15:52:12 +0100

    vercel correction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/server'; // <– adapter server
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server',          // important pour les endpoints /api
-  adapter: vercel(),         // <– on indique l’adapter
+  output: 'server',
+  adapter: vercel({
+    // mode: 'standalone' // optionnel, tu peux laisser vide au début
+  }),
   integrations: [react()],
 });
