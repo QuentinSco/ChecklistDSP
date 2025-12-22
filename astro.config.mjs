d210e3e1f8b845349d71ad09ee56f4962c6fa065
QuentinSco <q.scozzafave@gmail.com>
2025-12-22 15:50:24 +0100

    Ajout Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/server'; // <– adapter server
 
 export default defineConfig({
+  output: 'server',          // important pour les endpoints /api
+  adapter: vercel(),         // <– on indique l’adapter
   integrations: [react()],
 });
